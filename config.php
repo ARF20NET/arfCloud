78beb2d7b16aeeb6ab6429425c18f71a7a29cb36
arf20 <35542215+arf20@users.noreply.github.com>
2022-03-19 14:04:27 +0100

    Provide example config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+// Example config.php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'hostname');
+define('DB_USERNAME', 'username');
+define('DB_PASSWORD', 'password');
+define('DB_NAME', 'dbname');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
